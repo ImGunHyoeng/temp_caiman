8abbfac60a539181aa687a53bd0a4c443ff0fe14
ImGunHyoeng <ynoe30@gmail.com>
2023-09-14 15:23:56 +0900

    move and jump build

--- Source/Caiman/CCharacterPlayer.h
+++ Source/Caiman/CCharacterPlayer.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "CCharacterBase.h"
+#include "GameFramework/character.h"
+#include "InputActionValue.h"
 #include "CCharacterPlayer.generated.h"
 
 /**
@@ -21,4 +23,26 @@ public:
 		TObjectPtr<class UCameraComponent>Camera;//카메라
 
 	ACCharacterPlayer();
+	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = Input)
+		class UInputMappingContext* PlayerContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<class UInputAction> MovementAction;
+	/*UPROPERTY(EditAnywhere,BluePrintReadOnly, Category = Input)
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, BluePrintReadOnly, Category = Input)
+	UInputAction* JumpAction;*/
+	virtual void BeginPlay() override;
+	void Move(const FInputActionValue& Value);
+	//void Look(const FInputActionValue& Value);
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//virtual void SetCharacterControlData(const UABCharacterControlDataAsset* CharacterControlData);
 };
