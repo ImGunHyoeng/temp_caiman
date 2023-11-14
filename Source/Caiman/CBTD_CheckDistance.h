61ac7d4a1bd9f8424b457c49f58d91dd7aada239
ImGunHyoeng <ynoe30@gmail.com>
2023-11-14 15:22:50 +0900

    add BT patroll backPlayerPosition

---
+++ Source/Caiman/CBTD_CheckDistance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "CBTD_CheckDistance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAIMAN_API UCBTD_CheckDistance : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
